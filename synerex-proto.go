1725336e5f19a0140061ef27054d18c9369a900a
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2019-09-22 02:11:04 +0900

    add proto

--- synerex-proto.go
+++ synerex-proto.go
@@ -2,10 +2,10 @@ package proto // import "github.com/synerex/synerex_proto"
 //
 
 const (
-	RIDE_SHARE int32 = 1001  // Rideshare Service Information
-	AD_SERVICE int32 = 1002  // Advertisement Service Information
-	LIB_SERVICE int32 = 1003 // Public Library Service Information
-	PT_SERVICE int32 = 1004  // Public Transit Information
-	ROUTING_SERVICE int32 = 1005 // Routing Service
-	MARKETING_SERVICE int32 = 1006 // Marketing (Ad/Enquate)
+	RIDE_SHARE uint32 = 1001  // Rideshare Service Information
+	AD_SERVICE uint32 = 1002  // Advertisement Service Information
+	LIB_SERVICE uint32 = 1003 // Public Library Service Information
+	PT_SERVICE uint32 = 1004  // Public Transit Information
+	ROUTING_SERVICE uint32 = 1005 // Routing Service
+	MARKETING_SERVICE uint32 = 1006 // Marketing (Ad/Enquate)
 )
