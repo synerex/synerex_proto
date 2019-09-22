caa574d9d1359fee65b14650f6970be2e763bf88
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2019-09-22 12:57:39 +0900

    update version v0.1.1

--- synerex-proto.go
+++ synerex-proto.go
@@ -1,11 +1,16 @@
 package proto // import "github.com/synerex/synerex_proto"
 //
 
+const ChannelTypeVersion = "0.1.1"  // string for pbase version
+
+// Channel Types
 const (
-	RIDE_SHARE uint32 = 1001  // Rideshare Service Information
-	AD_SERVICE uint32 = 1002  // Advertisement Service Information
-	LIB_SERVICE uint32 = 1003 // Public Library Service Information
-	PT_SERVICE uint32 = 1004  // Public Transit Information
-	ROUTING_SERVICE uint32 = 1005 // Routing Service
-	MARKETING_SERVICE uint32 = 1006 // Marketing (Ad/Enquate)
+	RIDE_SHARE uint32 = 1  // Rideshare Service Information
+	AD_SERVICE uint32 = 2  // Advertisement Service Information
+	LIB_SERVICE uint32 = 3 // Public Library Service Information
+	PT_SERVICE uint32 = 4  // Public Transit Information
+	ROUTING_SERVICE uint32 = 5 // Routing Service
+	MARKETING_SERVICE uint32 = 6 // Marketing (Ad/Enquate)
 )
+
+const ChannelTypeMax = 20  // may double default channel size
