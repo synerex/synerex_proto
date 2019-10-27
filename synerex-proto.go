d8fa706f7adc382c722779e1b4e644f1f1bfc568
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2019-10-28 01:06:39 +0900

    add fluentd protocol

--- synerex-proto.go
+++ synerex-proto.go
@@ -1,19 +1,18 @@
 package proto // import "github.com/synerex/synerex_proto"
 //
 
-const ChannelTypeVersion = "0.1.1"  // string for pbase version
+const ChannelTypeVersion = "0.1.1" // string for pbase version
 
 // if you change this number you should update "ChannelTypeVersion"
-const ChannelTypeMax = 20  // Default Synerex Server channel size
+const ChannelTypeMax = 20 // Default Synerex Server channel size
 
 // Channel Types
 const (
-	RIDE_SHARE uint32 = 1  // Rideshare Service Information
-	AD_SERVICE uint32 = 2  // Advertisement Service Information
-	LIB_SERVICE uint32 = 3 // Public Library Service Information
-	PT_SERVICE uint32 = 4  // Public Transit Information
-	ROUTING_SERVICE uint32 = 5 // Routing Service
+	RIDE_SHARE        uint32 = 1 // Rideshare Service Information
+	AD_SERVICE        uint32 = 2 // Advertisement Service Information
+	LIB_SERVICE       uint32 = 3 // Public Library Service Information
+	PT_SERVICE        uint32 = 4 // Public Transit Information
+	ROUTING_SERVICE   uint32 = 5 // Routing Service
 	MARKETING_SERVICE uint32 = 6 // Marketing (Ad/Enquate)
+	FLUENTD_SERVICE   uint32 = 7 // Fluentd Service (td-agent/fluetnd)
 )
-
-
